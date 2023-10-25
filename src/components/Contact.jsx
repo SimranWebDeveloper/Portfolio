d2b3597accd6405c6b8b3969c3e2b96ea9b40ee4
Simran <simran.abbasov.00@mail.ru>
2023-10-25 16:19:31 +0400

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,135 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import { NavLink } from "react-router-dom";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "JavaScript Mastery",
+          from_email: form.email,
+          to_email: "sujata@jsmastery.pro",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
+
+  return (
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+
+
+
+          <label className='flex flex-col'>
+          <span className='text-white font-medium mt-4'>My name and surname</span>
+            <input
+              type='text'
+              name='name'
+              value='Simran Abbasov'
+              onChange={handleChange}
+              disabled
+              placeholder="What's your good name?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+              <span className='text-white font-medium mt-4'>My address</span>
+              <NavLink to='https://maps.app.goo.gl/StxbiNtdw2Ypz67K7' target="_blank"
+             className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'>Baku, Azerbaijan</NavLink>
+          <span className='text-white font-medium mt-4'>My email</span>
+            <NavLink to='mailto:simran.abb@div.edu.az' target="_blank"
+             className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'>simran.abb@div.edu.az</NavLink>
+            <span className='text-white font-medium mt-4'>My github</span>
+            <NavLink to='https://github.com/SimranWebDeveloper' target="_blank"
+             className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'>github.com/SimranWebDeveloper</NavLink>
+             <span className='text-white font-medium mt-4'> My linkedin</span>
+                         <NavLink to='https://www.linkedin.com/in/simranabbasov' target="_blank"
+             className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'>www.linkedin.com/in/simranabbasov</NavLink>
+                         <span className='text-white font-medium mt-4'>My number</span>
+            <NavLink to='tel:+994516944794'
+             className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'>+ (994) 51 694 47 94</NavLink>
+
+          </label>
+
+          
+          
+
+
+
+
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
